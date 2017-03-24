2a72b0210533b1b41a7a3fa4399e1a6bdd2d129b
Jim Huang <jserv.tw@gmail.com>
2017-03-24 22:32:40 +0800

    Trivial style tweaks

--- matrix.h
+++ matrix.h
@@ -3,7 +3,7 @@
 
 #include <stdbool.h>
 
-/* predefined */
+/* predefined shortcut */
 typedef struct { float values[3][3]; } Mat3x3;
 typedef struct { float values[4][4]; } Mat4x4;
 
@@ -12,12 +12,11 @@ typedef struct {
     void *priv;
 } Matrix;
 
-typedef struct __matrix_algo MatrixAlgo;
-struct __matrix_algo {
+typedef struct {
     void (*assign)(Matrix *thiz, Mat4x4);
     bool (*equal)(const Matrix *l, const Matrix *r);
     bool (*mul)(Matrix *dst, const Matrix *l, const Matrix *r);
-};
+} MatrixAlgo;
 
 /* Available matrix providers */
 extern MatrixAlgo NaiveMatrixProvider;
